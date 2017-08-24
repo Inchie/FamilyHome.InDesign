2b7f295d27427b5981349cd79fcdfd85456bb938
Timo Nußbaum <timo.nussbaum@gmx.de>
2017-08-24 15:42:01 +0200

    Task: Add sync for mac.

---
+++ sync_mac/config.jsx
@@ -0,0 +1,1 @@
+﻿/** *  This file is a config file for nutware *//* Base url to the application */var baseUrl = "https://www.nutware.de";/* uploads folder - webdav - for windows */var uploadsFolder = "/Volumes/webdav/uploads";
